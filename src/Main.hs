b0d314860639eb8aab2ade456eb3eb69a1713b28
Filip Brcic <brcha@yandex.com>
2020-06-22 21:08:21 +0200

    Applied hlint

--- src/Main.hs
+++ src/Main.hs
@@ -1,20 +1,19 @@
-{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE OverloadedStrings #-}
 
 module Main where
 
-import           GHC.Generics
+import GHC.Generics
 
-import           Data.Aeson
+import Data.Aeson
 
-data CovidEntry = CovidEntry
-    { confirmed    :: Int
-    , fatal        :: Int
-    , hospitalized :: Int
-    , ventilator   :: Int
-    , recovered    :: Int
-    }
-    deriving (Generic, Show)
+data CovidEntry = CovidEntry {
+    confirmed :: Int
+  , fatal :: Int
+  , hospitalized :: Int
+  , ventilator :: Int
+  , recovered :: Int
+} deriving (Generic, Show)
 
 instance ToJSON CovidEntry where
   toEncoding = genericToEncoding defaultOptions
@@ -23,7 +22,7 @@ instance FromJSON CovidEntry
 
 main :: IO ()
 main = do
-  let testEntry =
-        "{\"tested\":345251,\"testedO\":345260,\"confirmed\":12990,\"fatal\":262,\"hospitalized\":731,\"ventilator\":19,\"recovered\":11997,\"active\":731,\"caseP\":\"3.76\",\"fatalP\":\"2.02\",\"p0\":108,\"vs\":99,\"f0\":94}"
-      covEntry = decode testEntry :: Maybe CovidEntry
+  let
+    testEntry = "{\"tested\":345251,\"testedO\":345260,\"confirmed\":12990,\"fatal\":262,\"hospitalized\":731,\"ventilator\":19,\"recovered\":11997,\"active\":731,\"caseP\":\"3.76\",\"fatalP\":\"2.02\",\"p0\":108,\"vs\":99,\"f0\":94}"
+    covEntry = decode testEntry :: Maybe CovidEntry
   putStrLn $ show covEntry
