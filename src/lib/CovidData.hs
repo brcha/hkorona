f86dbb72fab73e3fd933f1e9a2ca293cbd6b45bd
Filip Brcic <brcha@yandex.com>
2020-06-24 22:25:16 +0200

    Implemented CovidEntry

--- src/lib/CovidData.hs
+++ src/lib/CovidData.hs
@@ -1,14 +1,17 @@
-{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE DeriveGeneric     #-}
+{-# LANGUAGE OverloadedStrings #-}
 
 -- This module models the JSON data from https://github.com/urosevic/covid19/
 module CovidData
   ( CovidEntryTotals(..)
+  , CovidEntry(..)
   ) where
 
 import           GHC.Generics
 
 import           Data.Aeson
 
+import           Control.Applicative
 import           Data.Time
 
 -- Totals for a single entry in the covid json file
@@ -25,6 +28,27 @@ instance ToJSON CovidEntryTotals where
   toEncoding = genericToEncoding defaultOptions
 
 instance FromJSON CovidEntryTotals
+
+-- Actual entry in the JSON file
+data CovidEntry = CovidEntry
+    { date   :: Day
+    , totals :: CovidEntryTotals
+    }
+    deriving (Generic, Show, Eq)
+
+instance ToJSON CovidEntry where
+  toEncoding = genericToEncoding defaultOptions
+
+instance FromJSON CovidEntry where
+  parseJSON (Object v) = do
+    date' <- v .: "date"
+    totals' <- v .: "totals"
+    let (day, rest) = span (/= '/') date'
+        (month, rest') = span (/= '/') $ tail rest
+        (year, _) = span (/= ' ') $ tail rest'
+        fixedDate = fromGregorian (read year) (read month) (read day)
+    return (CovidEntry {date = fixedDate, totals = totals'})
+  parseJSON _ = empty
 
 data CovidData = CovidData
     { updated :: UTCTime
--- tests/Main.hs
+++ tests/Main.hs
@@ -9,10 +9,13 @@ import           CovidData
 
 import           Data.Aeson
 
+import           Data.Time
+
 main :: IO ()
 main = do
-  ces <- testSpec "CovidEntryTotals" covidEntryTotalsSpec
-  defaultMain (testGroup "Tests" [ces])
+  cets <- testSpec "CovidEntryTotals" covidEntryTotalsSpec
+  ces <- testSpec "CovidEntry" covidEntrySpec
+  defaultMain (testGroup "Tests" [cets, ces])
 
 -- Enable QuickCheck for CovidEntryTotals
 instance Arbitrary CovidEntryTotals where
@@ -38,3 +41,23 @@ covidEntryTotalsSpec =
               , recovered = 11997
               }
        in decodedEntry `shouldBe` Just expectedEntry
+
+covidEntrySpec :: Spec
+covidEntrySpec =
+  parallel $ do
+    it "Check if reading date works correctly" $
+      let entryTotalsData =
+            CovidEntryTotals
+              { confirmed = 13235
+              , fatal = 263
+              , hospitalized = 861
+              , ventilator = 21
+              , recovered = 12111
+              }
+          entryData =
+            CovidEntry
+              {date = fromGregorian 2020 6 24, totals = entryTotalsData}
+          testEntry =
+            "{\"date\":\"24/06/2020 15:00\",\"dateTime\":{\"date\":\"24.06.2020\",\"time\":\"15:00\"},\"totals\":{\"tested\":359060,\"testedO\":359069,\"confirmed\":13235,\"fatal\":263,\"hospitalized\":861,\"ventilator\":21,\"recovered\":12111,\"active\":861,\"caseP\":\"3.69\",\"fatalP\":\"1.99\",\"p0\":110,\"vs\":101,\"f0\":96},\"details\":{\"positive\":143,\"hospital\":143,\"personal\":0,\"negative\":7641,\"totalTested\":7784,\"fatalM\":0,\"fatalF\":0,\"fatal\":0,\"recovered\":57,\"caseP\":\"1.84\"},\"info\":{\"refs\":[\"https://www.zdravlje.gov.rs/vest/348407/informacija-o-novom-korona-virusu-na-dan-24-jun-2020-godine-u-15-casova.php\"],\"note\":\"\"}}"
+          decodedEntry = decode testEntry :: Maybe CovidEntry
+       in decodedEntry `shouldBe` Just entryData
